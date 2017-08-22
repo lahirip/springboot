afa8cdaf86fdba305cf63254bafaafd074d2f9fe
pallab <pallab.lahiri@gmail.com>
2017-08-21 23:19:31 -0700

    adding service registry

---
+++ service-registry/README.md
@@ -0,0 +1,3 @@
+This is a simple service registry based on Netflix Eureka
+
+Nothing other than @EnableEurekaServer  and setting up the application.properties are required here.
---
+++ service-registry/src/main/java/com/pl/boot/serviceregistry/ServiceRegistryApplication.java
@@ -0,0 +1,15 @@
+package com.pl.boot.serviceregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class ServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistryApplication.class, args);
+	}
+}
